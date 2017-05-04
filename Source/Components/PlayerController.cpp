f7ee7227d85cd2e8ffb4f4156ff50da16daf6f42
Alex Pettersson <alpe0504@gmail.com>
2017-05-04 17:29:41 +0200

    now has both keyboard and xbox input support

--- Source/Components/PlayerController.cpp
+++ Source/Components/PlayerController.cpp
@@ -1,16 +1,183 @@
 #include "stdafx.h"
 #include "PlayerController.h"
+#include "..\CommonUtilities\InputMessage.h"
+#include "..\CommonUtilities\EInputReturn.h"
+#include "..\CommonUtilities\XInputWrapper.h"
+#include "..\CommonUtilities\EKeyboardKeys.h"
+#include "KartControllerComponent.h"
 
 
 CPlayerController::CPlayerController(CKartControllerComponent & aKartComponent) : CController(aKartComponent)
 {
+	myIsMovingBackwards = false;
+	myIsMovingFoward = false;
+	myIsTurningLeft = false;
+	myIsTurningRight = false;
 }
 
 CPlayerController::~CPlayerController()
 {
 }
 
-void CPlayerController::Update()
+void CPlayerController::Update(const float aDeltaTime)
 {
 	//kolla input och kalla på functioner hos KartControllerComponent
+}
+
+CU::eInputReturn CPlayerController::TakeInput(const CU::SInputMessage & aInputMessage)
+{
+	if (aInputMessage.myType == CU::eInputType::eGamePadButtonPressed)
+	{
+		GamePadPressedKey(aInputMessage);
+	}
+	else if (aInputMessage.myType == CU::eInputType::eGamePadButtonReleased)
+	{
+		GamePadReleasedKey(aInputMessage);
+	}
+	else if (aInputMessage.myType == CU::eInputType::eGamePadLeftJoyStickChanged)
+	{
+		MovedJoystick(aInputMessage);
+	}
+	else if (aInputMessage.myType == CU::eInputType::eKeyboardReleased)
+	{
+		ReleasedKey(aInputMessage);
+	}
+	else if (aInputMessage.myType == CU::eInputType::eKeyboardPressed)
+	{
+		PressedKey(aInputMessage);
+	}
+
+	return CU::eInputReturn::ePassOn;
+}
+
+void CPlayerController::ReleasedKey(const CU::SInputMessage & aInputMessage)
+{
+	switch (aInputMessage.myKey)
+	{
+	case CU::eKeys::UP:
+	case CU::eKeys::W:
+		myControllerComponent.StopMoving();
+		myIsMovingFoward = false;
+		if (myIsMovingBackwards == true)
+		{
+			myControllerComponent.MoveBackWards();
+		}
+		break;
+	case CU::eKeys::DOWN:
+	case CU::eKeys::S:
+		myControllerComponent.StopMoving();
+		myIsMovingBackwards = false;
+		if (myIsMovingFoward == true)
+		{
+			myControllerComponent.MoveFoward();
+		}
+		break;
+	case CU::eKeys::LEFT:
+	case CU::eKeys::A:
+		myControllerComponent.StopTurning();
+		myIsTurningLeft = false;
+		if (myIsTurningRight == true)
+		{
+			myControllerComponent.TurnRight();
+		}
+		break;
+	case CU::eKeys::RIGHT:
+	case CU::eKeys::D:
+		myControllerComponent.StopTurning();
+		myIsTurningRight = false;
+		if (myIsTurningLeft == true)
+		{
+			myControllerComponent.TurnLeft();
+		}
+		break;
+	}
+}
+
+void CPlayerController::PressedKey(const CU::SInputMessage & aInputMessage)
+{
+	switch (aInputMessage.myKey)
+	{
+	case CU::eKeys::UP:
+	case CU::eKeys::W:
+		myControllerComponent.MoveFoward();
+		myIsMovingFoward = true;
+		break;
+	case CU::eKeys::DOWN:
+	case CU::eKeys::S:
+		myControllerComponent.MoveBackWards();
+		myIsMovingBackwards = true;
+		break;
+	case CU::eKeys::LEFT:
+	case CU::eKeys::A:
+		myControllerComponent.TurnLeft();
+		myIsTurningLeft = true;
+		break;
+	case CU::eKeys::RIGHT:
+	case CU::eKeys::D:
+		myControllerComponent.TurnRight();
+		myIsTurningRight = true;
+		break;
+	}
+}
+
+void CPlayerController::GamePadPressedKey(const CU::SInputMessage & aInputMessage)
+{
+	switch (aInputMessage.myGamePad)
+	{
+	case CU::GAMEPAD::A:
+		myControllerComponent.MoveFoward();
+		myIsMovingFoward = true;
+		break;
+	case CU::GAMEPAD::B:
+		myControllerComponent.MoveBackWards();
+		myIsMovingBackwards = true;
+		break;
+	}
+}
+
+void CPlayerController::GamePadReleasedKey(const CU::SInputMessage & aInputMessage)
+{
+	switch (aInputMessage.myGamePad)
+	{
+	case CU::GAMEPAD::A:
+		myControllerComponent.StopMoving();
+		myIsMovingFoward = false;
+		if (myIsMovingBackwards == true)
+		{
+			myControllerComponent.MoveBackWards();
+		}
+		break;
+	case CU::GAMEPAD::B:
+		myControllerComponent.StopMoving();
+		myIsMovingBackwards = false;
+		if (myIsMovingFoward == true)
+		{
+			myControllerComponent.MoveFoward();
+		}
+		break;
+	}
+}
+
+void CPlayerController::MovedJoystick(const CU::SInputMessage & aInputMessage)
+{
+	if (aInputMessage.myJoyStickPosition.x == 0.f)
+	{
+		myControllerComponent.StopTurning();
+		return;
+	}
+
+	if (aInputMessage.myJoyStickPosition.x > 0)
+	{
+		myControllerComponent.TurnRight();
+	}
+	else if(aInputMessage.myJoyStickPosition.x < 0)
+	{
+		myControllerComponent.TurnLeft();
+	}
+
+}
+
+void CPlayerController::JoystickDeadzone()
+{
+	myControllerComponent.StopTurning();
 }
--- Source/Components/PlayerController.h
+++ Source/Components/PlayerController.h
@@ -1,13 +1,31 @@
 #pragma once
 #include "Controller.h"
-class CPlayerController : public CController
+#include "../CommonUtilities/InputListener.h"
+class CPlayerController : public CController, public CU::IInputListener
 {
 public:
 	CPlayerController(CKartControllerComponent& aKartComponent);
 	~CPlayerController();
 
-	void Update() override;
+	void Update(const float aDeltaTime) override;
+
+	virtual CU::eInputReturn TakeInput(const CU::SInputMessage& aInputMessage) override;
 
 private:
+
+	void ReleasedKey(const CU::SInputMessage& aInputMessage);
+	void PressedKey(const CU::SInputMessage& aInputMessage);
+	void GamePadPressedKey(const CU::SInputMessage& aInputMessage);
+	void GamePadReleasedKey(const CU::SInputMessage& aInputMessage);
+	void MovedJoystick(const CU::SInputMessage& aInputMessage);
+	void JoystickDeadzone();
+
+	bool myIsMovingFoward;
+	bool myIsMovingBackwards;
+	bool myIsTurningLeft;
+	bool myIsTurningRight;
+
+
+	float myLastTurnDirection;
 };
 
