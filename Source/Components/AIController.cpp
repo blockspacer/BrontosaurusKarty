9ecc4e5957be122cd4a01ab40dd3e27795a0eed1
Alex Pettersson <alpe0504@gmail.com>
2017-05-04 09:16:58 +0200

    AI controller for later implementation

---
+++ Source/Components/AIController.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "AIController.h"
+
+
+CAIController::CAIController(CKartControllerComponent& aKartComponent) : CController(aKartComponent)
+{
+}
+
+
+CAIController::~CAIController()
+{
+}
+
+void CAIController::Update()
+{
+	//beteende för AIn sen kalla på myControllerComponents funktioner för att flytta objektet
+}
