1019055bd2597111e202463d5a22987fe7ba8969
Kyle Thorburn <kyle.thorburn.sp15@thegameassembly.com>
2017-05-12 15:55:52 +0200

    removed countdown from polling

--- Source/Game/PollingStation.h
+++ Source/Game/PollingStation.h
@@ -8,15 +8,11 @@ public:
 	static void Create();
 	static void Destroy();
 	static CPollingStation* GetInstance()			{ return ourInstance; }
-	unsigned char GetStartCountdownTime()	{ return myStartCountdownTime; }
 private:
 	CPollingStation();
 	~CPollingStation();
 
-	inline void SetStartCountDownTime(unsigned char aTime) { myStartCountdownTime = aTime; }
-
 private:
 	static CPollingStation* ourInstance;
-	unsigned char myStartCountdownTime;
 };
 
