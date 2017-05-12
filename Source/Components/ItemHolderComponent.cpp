d88635c0d1fa1919ef311429671786cd77db7dcc
Alex Pettersson <alpe0504@gmail.com>
2017-05-12 10:08:40 +0200

    Item holder component

---
+++ Source/Components/ItemHolderComponent.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "ItemHolderComponent.h"
+
+
+CItemHolderComponent::CItemHolderComponent()
+{
+}
+
+
+CItemHolderComponent::~CItemHolderComponent()
+{
+}
+
+void CItemHolderComponent::Receive(const eComponentMessageType aMessageType, const SComponentMessageData & aMessageData)
+{
+
+}
