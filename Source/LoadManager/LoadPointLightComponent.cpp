f743fa2af4db29ad9ed398c608764d70b9d7f0b8
Edvin Jacobsson <resanderum@gmail.com>
2017-05-29 14:43:32 +0200

    LoadLights and stuff

--- Source/LoadManager/LoadPointLightComponent.cpp
+++ Source/LoadManager/LoadPointLightComponent.cpp
@@ -4,15 +4,25 @@
 #include "LoadManager.h"
 #include "ComponentManager.h"
 #include "PointLightComponent.h"
-#include "PointLightComponentManager.h"
+#include "SpotLightComponent.h"
+#include "LightComponentManager.h"
 #include "GameObject.h"
 
 int LoadPointLightComponent(KLoader::SLoadedComponentData someData)
 {
-
-	if (someData.myData.HasKey("range"))
+	if (someData.myData.HasKey("spotAngle"))
 	{
-		PointLightComponent* pointLight = CPointLightComponentManager::GetInstance().CreateAndRegisterComponent();
+		CSpotLightComponent* spotLight = CLightComponentManager::GetInstance().CreateAndRegisterSpotLightComponent();
+		spotLight->SetColor(CU::Vector3f(someData.myData.at("color").GetVector4f("xyzw") / 255.f));
+		spotLight->SetIntensity(someData.myData.at("intensity").GetFloat());
+		spotLight->SetRange(someData.myData.at("range").GetFloat());
+		spotLight->SetSpotAngle(someData.myData.at("spotAngle").GetFloat() * (3.141692f / 180.f));
+
+		return spotLight->GetId();
+	}
+	else if (someData.myData.HasKey("range"))
+	{
+		PointLightComponent* pointLight = CLightComponentManager::GetInstance().CreateAndRegisterPointLightComponent();
 		pointLight->SetColor(CU::Vector3f(someData.myData.at("color").GetVector4f("xyzw") / 255.f));
 		pointLight->SetIntensity(someData.myData.at("intensity").GetFloat());
 		pointLight->SetRange(someData.myData.at("range").GetFloat());
@@ -26,7 +36,7 @@ int LoadPointLightComponent(KLoader::SLoadedComponentData someData)
 		directionalLight.direction = -parent->GetToWorldTransform().myForwardVector;
 		directionalLight.intensity = someData.myData.at("intensity").GetFloat();
 		directionalLight.color = someData.myData.at("color").GetVector4f("xyzw") / 255.f;
-		CPointLightComponentManager::GetInstance().AddDirectionalLightToScene(directionalLight);
+		CLightComponentManager::GetInstance().AddDirectionalLightToScene(directionalLight);
 
 		return NULL_COMPONENT;
 	}
