02198cf935149967710ec0a6d97664860d6152e0
carlberyeur <carlcarlberger@gmail.com>
2017-05-30 09:58:46 +0200

    ai also has animated kart now removed some lava flow

--- Source/Game/PlayState.cpp
+++ Source/Game/PlayState.cpp
@@ -567,7 +567,7 @@ void CPlayState::CreatePlayer(CU::Camera& aCamera, const SParticipant::eInputDev
 void CPlayState::CreateAI()
 {
 	CGameObject* secondPlayerObject = myGameObjectManager->CreateGameObject();
-	CModelComponent* playerModel = myModelComponentManager->CreateComponent("Models/Meshes/M_Kart_01.fbx");
+	CModelComponent* playerModel = myModelComponentManager->CreateComponent("Models/Animations/M_Kart_01.fbx");
 
 	secondPlayerObject->AddComponent(playerModel);
 	secondPlayerObject->AddComponent(new Component::CKartModelComponent(myPhysicsScene));
