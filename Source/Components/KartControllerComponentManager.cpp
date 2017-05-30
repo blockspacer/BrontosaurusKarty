7289372c6f6a67f97a560fa47410fa9e5728239b
Alex Pettersson <alpe0504@gmail.com>
2017-05-30 12:58:39 +0200

    Added a get index number from closes spline

--- Source/Components/KartControllerComponentManager.cpp
+++ Source/Components/KartControllerComponentManager.cpp
@@ -80,6 +80,29 @@ const CU::Vector3f CKartControllerComponentManager::GetClosestSpinesDirection(co
 	return direction;
 }
 
+const int CKartControllerComponentManager::GetClosestSpinesIndex(const CU::Vector3f& aKartPosition)
+{
+	float distance = 999999;
+	unsigned int index = -1;
+
+	for (unsigned int i = 0; i < myNavigationSpline.GetNumberOfPoints(); i++)
+	{
+		float newDistance = CU::Vector2f(myNavigationSpline.GetPoint(i).myPosition - CU::Vector2f(aKartPosition.x, aKartPosition.z)).Length2();
+		if (newDistance < distance)
+		{
+			distance = newDistance;
+			index = i;
+		}
+	}
+
+	if (index > myNavigationSpline.GetNumberOfPoints())
+	{
+		return 0;
+	}
+
+	return index;
+}
+
 const SNavigationPoint* CKartControllerComponentManager::GetNavigationPoint(const int aIndex)
 {
 	if(aIndex < myNavigationSpline.GetNumberOfPoints())
