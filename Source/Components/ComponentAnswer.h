1d8fb99f01a6ae4efafa1189429e9c6f8f5701d6
Kyle Thorburn <kyle.thorburn.sp15@thegameassembly.com>
2017-06-07 11:03:01 +0200

    comp answer char info

--- Source/Components/ComponentAnswer.h
+++ Source/Components/ComponentAnswer.h
@@ -3,6 +3,7 @@
 
 class CColliderComponent;
 struct SNavigationPoint;
+struct SCharacterInfo;
 
 namespace CU
 {
@@ -48,6 +49,7 @@ enum class eComponentQuestionType
 	eGetCurrentSpline,
 	eGetRespawnSplineWithIndex,
 	eGetTerrainModifier,
+	eGetCharacterInfo,
 	eLength,
 };
 struct SComponentQuestionData
@@ -69,6 +71,7 @@ struct SComponentQuestionData
 		const char* myString;
 		CGameObject* myGameObject;
 		const SNavigationPoint* myNavigationPoint;
+		SCharacterInfo* myCharacterInfo;
 	};
 
 };
