27dbc033628a35ec071a5ae16e04633d45220111
Johan <johan.hagelqvist.sp15@thegameassembly.com>
2017-06-07 16:23:44 +0200

    you can now select characters with the keyboard arrow keys

--- Source/Game/SplitScreenSelection.cpp
+++ Source/Game/SplitScreenSelection.cpp
@@ -309,6 +309,53 @@ CU::eInputReturn CSplitScreenSelection::RecieveInput(const CU::SInputMessage & a
 				participant.myInputDevice = SParticipant::eInputDevice::eKeyboard;
 			}
 			break;
+		case CU::eKeys::UP:
+		{
+			bool found = false;
+			for (unsigned int i = 0; i < myPlayers.Size(); ++i)
+			{
+				if (myPlayers[i].myInputDevice == SParticipant::eInputDevice::eKeyboard)
+				{
+					myPlayers[i].mySelectedCharacter = SParticipant::eCharacter::eVanBrat;
+				}
+			}
+		}
+		break;
+		case CU::eKeys::RIGHT:
+		{
+			bool found = false;
+			for (unsigned int i = 0; i < myPlayers.Size(); ++i)
+			{
+				if (myPlayers[i].myInputDevice == SParticipant::eInputDevice::eKeyboard)
+				{
+					myPlayers[i].mySelectedCharacter = SParticipant::eCharacter::eVanBrat2;
+				}
+			}
+		}
+		break;
+		case CU::eKeys::DOWN:
+		{
+			bool found = false;
+			for (unsigned int i = 0; i < myPlayers.Size(); ++i)
+			{
+				if (myPlayers[i].myInputDevice == SParticipant::eInputDevice::eKeyboard)
+				{
+					myPlayers[i].mySelectedCharacter = SParticipant::eCharacter::eGrandMa;
+				}
+			}
+		}
+		break;
+		case CU::eKeys::LEFT:
+		{
+			bool found = false;
+			for (unsigned int i = 0; i < myPlayers.Size(); ++i)
+			{
+				if (myPlayers[i].myInputDevice == SParticipant::eInputDevice::eKeyboard)
+				{
+					myPlayers[i].mySelectedCharacter = SParticipant::eCharacter::eGrandMa2;
+				}
+			}
+		}
 		case CU::eKeys::RETURN:
 			//Continue to map selection or game
 				break;
