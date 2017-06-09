935d9cd49001f5379b7fb205a0d921d6f519de8d
carlberyeur <carlcarlberger@gmail.com>
2017-06-09 10:00:09 +0200

    added assert to animation loading that the animations have same amount of bones as their bind pose

--- Source/Components/AIController.cpp
+++ Source/Components/AIController.cpp
@@ -24,7 +24,7 @@ CAIController::~CAIController()
 void CAIController::Update(const float aDeltaTime)
 {
 	UpdateItemUsage(aDeltaTime);
-	UpdateMovement(aDeltaTime);	
+	UpdateMovement(aDeltaTime);
 }
 
 void CAIController::UpdateWithoutItems(const float aDeltaTime)
