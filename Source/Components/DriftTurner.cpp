8d46e65b7be01b44354f9baacd11a944f8d75311
Edvin Jacobsson <resanderum@gmail.com>
2017-06-12 19:23:33 +0200

    Drift lerp thingy

--- Source/Components/DriftTurner.cpp
+++ Source/Components/DriftTurner.cpp
@@ -2,11 +2,54 @@
 #include "DriftTurner.h"
 #include "../Physics/PhysXHelper.h"
 
+enum class eLerpCurve : char
+{
+	eLinear,
+	eExponential,
+	eEaseOut,
+	eEaseIn,
+	eSmoothStep,
+	eSmootherStep,
+};
 
+namespace
+{
+	float fLerp(float aStart, float aEnd, float aTime)
+	{
+		return (aStart + aTime*(aEnd - aStart));
+		//return (aEnd + aTime*(aStart - aEnd));
+	}
+
+	float CalcCurve(float t, eLerpCurve aCurveType)
+	{
+		const float pi = 3.1415f;
+
+		switch (aCurveType)
+		{
+		case eLerpCurve::eLinear:
+			return t;
+		case eLerpCurve::eExponential:
+			return t * t;
+		case eLerpCurve::eEaseOut:
+			return std::sin(t * pi * 0.5f);
+		case eLerpCurve::eEaseIn:
+			return 1.f - std::cos(t * pi * 0.5f);
+		case eLerpCurve::eSmoothStep:
+			return t*t * (3.f - 2.f*t);
+		case eLerpCurve::eSmootherStep:
+			return t*t*t * (t * (6.f*t - 15.f) + 10.f);
+		default:
+			return 0.0f;
+			break;
+		}
+	}
+
+}
 Component::CDriftTurner::CDriftTurner()
 {
 	myHasBeenHit = false;
 	myHasBeenHitTotalTime = 0;
+	shouldLerp = false;
 }
 
 
@@ -21,13 +64,25 @@ void Component::CDriftTurner::Receive(const eComponentMessageType aMessageType, 
 	{
 	case eComponentMessageType::eDoDriftBobbing:
 	{
-		GetParent()->GetLocalTransform().SetRotation(CU::Matrix33f::CreateRotateAroundY(aMessageData.myFloat));
+		//GetParent()->GetLocalTransform().SetRotation(CU::Matrix33f::CreateRotateAroundY(aMessageData.myFloat));
 		//myTargetTurn = CU::Matrix33f::CreateRotateAroundY(aMessageData.myFloat);
-	}
+		myTargetAngle = aMessageData.myFloat;
+		myStartAngle = 0.0f;
+		myLerpTimer = 0.0f;
+		shouldLerp = true;
+		myCurve = eLerpCurve::eEaseIn;
+
 		break;
+	}
 	case eComponentMessageType::eCancelDriftBobbing:
 		GetParent()->GetLocalTransform().SetRotation(CU::Matrix33f::Identity);
 		myTargetTurn = CU::Matrix33f::Identity;
+		myLerpTimer = 0.0f;
+		myCurve = eLerpCurve::eEaseOut;
+
+		myStartAngle = myTargetAngle;
+		myTargetAngle = 0.0f;
+		shouldLerp = true;
 		break;
 	case eComponentMessageType::eUpdate:
 		DoUpdate(aMessageData.myFloat);
@@ -53,6 +108,17 @@ void Component::CDriftTurner::Destroy()
 const float sleprVal = 10.f;
 void Component::CDriftTurner::DoUpdate(const float aDeltaTime)
 {
+	myLerpTimer += aDeltaTime;
+	if (myLerpTimer / 0.1f >= 1.0f)
+	{
+		shouldLerp = false;
+	}
+	if (shouldLerp)
+	{
+		float angle = fLerp(myStartAngle, myTargetAngle, CalcCurve(CLAMP(myLerpTimer / 0.1f, 0.0f, 1.0f), myCurve));
+		GetParent()->GetLocalTransform().SetRotation(CU::Matrix33f::CreateRotateAroundY(angle));
+	}
+
 	//GetParent()->GetLocalTransform().SetRotation(Physics::Slerp(GetParent()->GetLocalTransform().GetRotation(), myTargetTurn, ( 1.f / sleprVal) * aFloat).GetRotation());
 	if (myHasBeenHit == true)
 	{
